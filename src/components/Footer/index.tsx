71e99894cd8b5f57e28de567ae8d32817532d3c6
Charles Borges <solucao.erp@gmail.com>
2023-09-24 21:37:00 -0300

    Final adjustments

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
         <div className="footer-container">
             <footer className="car-footer">
                 <p className="footer-text footer-text-left">@dscarrostop</p>
-                <p className="footer-text footer-text-right">Rua da Pipoca, 150</p>
+                <p className="footer-text footer-text-right"><i>Rua da Pipoca, 150</i></p>
             </footer>
         </div>
     );
